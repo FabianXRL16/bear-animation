f52c2fe66710a3159a5dc11081ee59c2a3f92c89
FabianXRL16 <fabianpacherres@gmail.com>
2022-01-15 22:46:32 -0500

    Add animation of circle

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let j = 2;
 function sound() {
   let on = document.querySelector(".on");
   let off = document.querySelector(".off");
+  let btn = document.querySelector(".circle");
   audio.volume = 0.1;
   audio.loop = true;
   audio.paused ? audio.play() : audio.pause();
@@ -10,10 +11,12 @@ function sound() {
     off.style.display = "none";
     on.style.display = "block";
     on.style.transform = "scale(1)";
+    btn.style.animationName = "turn";
   } else {
     on.style.display = "none";
     off.style.display = "block";
     off.style.transform = "scale(1)";
+    btn.style.animationName = "";
   }
   j = j + 1;
 }
